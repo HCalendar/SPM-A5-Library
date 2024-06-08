38654cefea183a5b5ae0d8f1e147c079660f5772
hmy667 <1912830365@qq.com>
2024-06-08 18:14:45 +0800

    Add files via upload

---
+++ libraryBack/src/main/java/com/spm/libraryback/mapper/StarMapper.java
@@ -0,0 +1,8 @@
+package com.spm.libraryback.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.spm.libraryback.entity.Star;
+
+public interface StarMapper extends BaseMapper<Star> {
+}
